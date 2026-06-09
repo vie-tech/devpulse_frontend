892237f80303e01eb65ded6e5a22daba1a67c59d
vie-tech <ativiefavour03@gmail.com>
2026-06-09 18:29:57 +0100

    bug ffix2!

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-      }
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [react()],
+        server: {
+            port: 5173,
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_BASE_URL,
+                    changeOrigin: true,
+                },
+            },
+        },
+    };
+});
